aac06f248bc2e7d5157df17f5865762c86213125
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2021-03-23 23:06:42 +0300

    fix translation and oopsie brawler

--- src/appConfig.js
+++ src/appConfig.js
@@ -3,7 +3,7 @@ export default {
 	defaultRegionParamOverride: "eu",
 	apiUrl: "http://127.0.0.1",
 	gameClasses: ["Warrior", "Lancer", "Slayer", "Berserker", "Sorcerer", "Archer", "Priest", "Mystic", "Reaper", "Gunner", "Brawler", "Ninja", "Valkyrie"],
-	tanksGameClasses: ["Lancer", "Warrior"],
+	tanksGameClasses: ["Lancer", "Warrior", "Brawler"],
 	healersGameClasses: ["Priest", "Mystic"],
 	allowedDungeons: []
 }
